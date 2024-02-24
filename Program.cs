e8d08377b556f61e0365ae8a901b7a2fa9c8d290
Gustavo Barrera <ggodoyb1@miumg.edu.gt>
2024-02-24 11:10:22 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿int contador = 0;
+int numero = 2;
+ Console.WriteLine("Los primeros 5 números pares son:");
+while (contador < 5)
+ {
+      Console.WriteLine(numero);
+      numero += 2; 
+      contador++;
+        }
+
+Console.ReadLine();
+
+
+
+int suma = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    suma += i; 
+}
+
+Console.WriteLine($"La suma de los primeros 10 números naturales es: {suma}");
+
+
+
+int suma = 0;
+int numero;
+
+Console.WriteLine("Ingresa números enteros positivos. Escribe un número negativo para finalizar.");
+
+do
+{
+    Console.Write("Número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero >= 0)
+    {
+        suma += numero;
+    }
+} while (numero >= 0);
+
+Console.WriteLine($"La suma de los números ingresados es: {suma}");
+
+
